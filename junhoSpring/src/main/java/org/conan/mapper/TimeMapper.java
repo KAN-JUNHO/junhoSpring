7f312772fee7cec6d147b36a75234dcab8b4c1d6
박준호 <56674106+KAN-JUNHO@users.noreply.github.com>
2022-05-26 17:56:13 +0900

    no message

--- junhoSpring/src/main/java/org/conan/mapper/TimeMapper.java
+++
@@ -1,9 +0,0 @@
-package org.conan.mapper;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface TimeMapper {
-	@Select("SELECT SYSDATE FROM DUAL")
-	public String getTime();	//annotation
-	public String getTime2();   //XML
-}
